312596ca91730973af3f1266d6d25f399522d032
sebasx200 <sebasx200@hotmail.com>
2025-03-10 16:25:34 -0500

    feat: now the page takes the vehicle model and does the following:
    - Lists all the vehicles in cards
    - Shows a default image depending on the type of vehicle
    - if no vehicles found, shows a message

---
+++ HotelMotor/Components/Pages/Vehicles/Vehicles.razor.cs
@@ -0,0 +1,34 @@
+﻿using HotelMotor.Modules.Vehicles;
+using HotelMotor.Modules.Vehicles.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace HotelMotor.Components.Pages.Vehicles
+{
+    public partial class Vehicles
+    {
+        [Inject]
+        private VehicleService VehicleService { get; set; }
+
+        private List<Vehicle> vehicles = new List<Vehicle>();
+
+        protected override void OnInitialized()
+        {
+            GetAllVehicles();
+        }
+
+        private List<Vehicle> GetAllVehicles()
+        {
+            return vehicles = VehicleService.GetVehicles();
+        }
+
+        private string GetVehicleImage(VehicleType vehicleType)
+        {
+            return vehicleType switch
+            {
+                VehicleType.Motorcycle => "images/vehicles/motos.jpg", // Reemplaza con la ruta de tu imagen de moto
+                VehicleType.Car => "images/vehicles/carros.jpg", // Reemplaza con la ruta de tu imagen de carro
+                _ => "images/vehicles/motoycarro.jpg" // Imagen predeterminada para otros tipos
+            };
+        }
+    }
+}
