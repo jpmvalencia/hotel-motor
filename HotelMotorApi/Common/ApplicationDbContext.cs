e2a0c68c79f6e375eba084331bb2f51488ae0488
Juan Pablo Méndez Valencia <juan_mendez23231@elpoli.edu.co>
2025-04-17 13:06:36 -0500

    feat(db): add basic models and migrations for Customer, Vehicle, Order, Service
    - Added DbSet properties in ApplicationDbContext
    - Created initial migration for the following entities:
      - Customer
      - Vehicle
      - Order
      - OrderService
      - Service
    - Included models in HotelMotorShared.Models
    - Updated .projitems to include new model files

--- HotelMotorApi/Common/ApplicationDbContext.cs
+++ HotelMotorApi/Common/ApplicationDbContext.cs
@@ -1,9 +1,16 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using HotelMotorShared.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace HotelMotorApi.Common
 {
     public class ApplicationDbContext : DbContext
     {
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Vehicle> Vehicles { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderService> OrderServices { get; set; }
+        public DbSet<Service> Services { get; set; }
+
         public ApplicationDbContext(DbContextOptions options) : base(options)
         {
         }
